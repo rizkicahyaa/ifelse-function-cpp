0ba2b587f62376a8aa0ac86b17eeefdc1a5a3fcf
Rizki Cahya <rizki.cahya03@gmail.com>
2025-09-02 17:12:16 +0700

    File: Function cpp

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void myFunction() {
+    cout << "Hello world" << endl;
+}
+
+int main() {
+    myFunction();
+    myFunction();
+    myFunction();
+    return 0;
+}
