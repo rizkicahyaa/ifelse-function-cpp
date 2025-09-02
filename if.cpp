0807280d8ae35675ee5316439787b2b388861c94
Rizki Cahya <rizki.cahya03@gmail.com>
2025-09-03 01:32:05 +0700

    Add: kasih penjelasan

--- if.cpp
+++ if.cpp
@@ -5,8 +5,10 @@ int main() {
     int umur = 18;
 
     if (umur >= 18) {
-        cout << "Anda sudah dewasa" << endl; // jika umur lebih dari 18 maka output akan muncul "Anda sudah dewasa"
-    } // dan jika sebaliknya maka output tidak akan muncul
+        // jika umur lebih dari 18 maka output akan muncul "Anda sudah dewasa"
+        cout << "Anda sudah dewasa" << endl; 
+    } 
+    // dan jika sebaliknya maka output tidak akan muncul
 
     return 0;
 }
--- ifelse.cpp
+++ ifelse.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 int main() {
-    int nilai;
+    int umur = 18;
 
-    cout << "Masukkan nilai kamu: ";
-    cin >> nilai;
-
-    if (nilai > 81) {
-        cout << "Nilai kamu A!" << endl;
+    if (umur >= 18) {
+        /* 
+        Jika umur lebih dari atau sama dengan 18, maka output nya adalah "Anda sudah dewasa"
+        karena kondisinya True
+        */
+        cout << "Anda sudah dewasa" << endl; 
     } else {
-        cout << "Nilai kamu B!" << endl;
+        /* 
+        Sebaliknya jika umur kurang dari 18, maka output nya adalah "Anda belum dewasa"
+        karena kondisinya False
+        */
+        cout << "Anda belum dewasa" << endl;
     }
 
     return 0;
